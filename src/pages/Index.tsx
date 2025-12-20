713a96f67e71c6de0535d791ef2849151cd4300c
Marwakhot <marwakhot007@gmail.com>
2025-12-20 14:33:28 +0400

    add

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -163,7 +163,7 @@ const Index = () => {
           <div className="container mx-auto px-6 flex flex-row items-center gap-12">
             {/* Left side - Title */}
             <motion.div 
-              className="w-1/3 flex-shrink-0 -mt-80"
+              className="w-1/3 flex-shrink-0 -mt-96"
               initial={{ opacity: 0, x: -50 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
